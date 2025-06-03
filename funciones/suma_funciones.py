5d8e8e8f4dd0aec612e8cdd74cb7c75731b975e6
Sw4ch1n <erreape0182@gmail.com>
2025-06-03 16:36:23 -0600

    Actualizo archivos .py tras limpieza en GitHub

--- funciones/suma_funciones.py
+++ funciones/suma_funciones.py
@@ -1,7 +1,14 @@
-def addition (a,b): #Definimos la funcion e indicamos los argumentos
-    print(f"La suma de {a} y {b} es {a + b}")
+def addition(a,b): #Definimos la funcion e indicamos los argumentos
+    return a + b #Retornamos la operacion
     
 numA = int(input("Ingresa un numero: "))
 numB = int(input("Ingresa otro numero: ")) #Solicitamos los numeros al usuario
 
-addition(numA,numB) #Llamamos a la funcion y le pasamos los argumentos que pide
+resultado = addition(numA,numB)#Llamamos a la funcion y le pasamos los argumentos que pide, igual se asigna a una variable para usarla en el print
+
+print(f"La suma de {numA} y {numB} es {resultado}")
+
+'''
+Usa return cuando quieras desacoplar la lógica de la presentación.
+la presentacoin son los prints
+'''
