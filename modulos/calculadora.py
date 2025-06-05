31ee5a771088cec6bdd9551e4a45e352161cb8ef
Sw4ch1n <erreape0182@gmail.com>
2025-06-04 18:47:34 -0600

    Actualizacion de los ejercicios modulos

--- modulos/calculadora.py
+++ modulos/calculadora.py
@@ -19,3 +19,19 @@ from calculadora import sumar, restar
 # Mostrar opciones, pedir números, ejecutar operación y mostrar resultado
 
 '''
+
+def sumar(a,b):
+    resultado_sum = a + b
+    return resultado_sum
+
+def restar(a,b): 
+    return a - b #Es una forma mas facil y con menos lineas de devolver un valor
+
+def multiplicar(a,b):
+    resultado_mult = a * b
+    return resultado_mult
+
+def dividir(a,b):
+    resultado_div = a / b
+    print(resultado_div)
+    return resultado_div
