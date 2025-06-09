2ee08cdf93b5f851e4623c9308e62d219bad4ced
Sw4ch1n <erreape0182@gmail.com>
2025-06-09 15:20:29 -0600

    Ejercicios POO, se crea la carpeta y archivo para trabajar

---
+++ bucles/mascota_virtual.py
@@ -0,0 +1,12 @@
+'''
+Crea un programa que permita representar una mascota. La mascota debe tener nombre, tipo (perro, gato...), y energía.
+Debe poder hacer las siguientes acciones:
+
+Comer (aumenta energía)
+
+Jugar (reduce energía)
+
+Mostrar estado (muestra nombre, tipo y energía actual)
+
+'''
+
